26f3437b3e54c99138a124ed258bf2e75f13bd8b
Alexander Y. <Emelin_A@t-online.de>
2025-03-01 19:06:04 +0100

    example dataset usage

--- ic50_tsv_app.py
+++ ic50_tsv_app.py
@@ -1,26 +1,47 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Set the page title and instructions
 st.title('MTT Assay IC50 Calculator')
 
 st.markdown('''
 ### Instructions:
-1. Upload a **TSV file** with MTT absorbance data (triplicates recommended).
+1. Upload a **TSV file** with MTT absorbance data (triplicates recommended), or use the example dataset.
 2. Specify the corresponding concentrations and controls.
-3. The app will generate the IC50 plot with error bars.
+3. The app will generate the IC50 plot with error bars and provide the estimated IC50 value.
 ''')
 
-# File uploader
-uploaded_file = st.file_uploader("Upload MTT Absorbance Data (TSV)", type="tsv")
+# Load the example dataset (use MTT_data.tsv)
+@st.cache_data
+def load_example_data():
+    example_file_path = 'MTT_data.tsv'  # Make sure the file is in the same folder as the script
+    return pd.read_csv(example_file_path, sep='\t')
 
-if uploaded_file:
-    df = pd.read_csv(uploaded_file, sep='\t')
+# File uploader & example data toggle
+use_example = st.checkbox("Use Example Data (MTT_data.tsv)")
 
-    st.write("### Preview of Uploaded Data")
+if use_example:
+    df = load_example_data()
+    st.success("Loaded example data.")
+else:
+    uploaded_file = st.file_uploader("Upload MTT Absorbance Data (TSV)", type="tsv")
+    if uploaded_file:
+        df = pd.read_csv(uploaded_file, sep='\t')
+
+# Proceed if data is available (either uploaded or example)
+if 'df' in locals():
+    st.write("### Preview of Data")
     st.write(df)
+
+    # Option to download the input data
+    st.download_button(
+        label="Download Input Data as TSV",
+        data=df.to_csv(sep='\t', index=False),
+        file_name="MTT_data_downloaded.tsv",
+        mime="text/tab-separated-values"
+    )
 
     # User input for concentrations and controls
     st.write("### Step 2: Define Experimental Conditions")
@@ -65,3 +86,4 @@ if uploaded_file:
     st.pyplot(fig)
 
     st.write(f"### Estimated IC50: {ic50_concentration:.2f} mg/ml")
+
